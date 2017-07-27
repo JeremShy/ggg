d0d8b0194191d85cbc49002b3edf79f7bd09685d
jcamhi <jeremshy@h3lp.fr>
2017-07-27 22:28:59 +0200

    Rainbow Dash is best pony !

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # ggg
+
+This is a very useful function to simplify your git push operations.
+
+Usage :
+
+ggg -> Add every files, commits with an automatic commit message and pushes.
+
+ggg "Commit message" -> Add every file, commits with the specified commit message, and pushes.
+
+ggg "./foo.c bar.c" "Commit message" -> Add specified files, commits with the specified commits message, and pushes.
---
+++ ggg.sh
@@ -0,0 +1,52 @@
+function ggg() {
+	if [ -n "$2" ]; then
+		git add $1
+		if [ "$?" != "0" ]; then
+			echo "Error while adding file $1"
+			return
+		fi
+		git commit -m $2
+		if [ "$?" != "0" ]; then
+			echo "Error while committing with commit message $2"
+			return
+		fi
+		git push
+		if [ "$?" != "0" ]; then
+			echo "Error while pushing"
+			return
+		fi
+	elif [ -n "$1" ]; then
+		git add -A
+		if [ "$?" != "0" ]; then
+			echo "Error while adding all files"
+			return
+		fi
+		git commit -m $1
+		if [ "$?" != "0" ]; then
+			echo "Error while committing with commit message $1"
+			return
+		fi
+		git push
+		if [ "$?" != "0" ]; then
+			echo "Error while pushing"
+			return
+		fi
+	else
+		git add -A
+		if [ "$?" != "0" ]; then
+			echo "Error while adding all files"
+			return
+		fi
+		git commit -m "Automatic commit !"
+		if [ "$?" != "0" ]; then
+			echo "Error while committing with commit message \"Automatic Commit\""
+			return
+		fi
+		git push
+		if [ "$?" != 0 ]; then
+			echo "Error while pushing with commit message \"Automatic Commit\""
+			return
+		fi
+	fi
+	echo "Everything went well !"
+}
