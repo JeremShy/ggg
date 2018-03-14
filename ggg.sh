e35ad1c8eefd8af463b29f3c4f88cb7d8c284509
jcamhi <jeremshy@h3lp.fr>
2018-03-14 22:16:51 +0100

    Automatic commit

--- ggg.sh
+++ ggg.sh
@@ -1,52 +1,50 @@
 function ggg() {
-	if [ -n "$2" ]; then
-		git add $1
-		if [ "$?" != "0" ]; then
-			echo "Error while adding file $1"
-			return
-		fi
-		git commit -m $2
-		if [ "$?" != "0" ]; then
-			echo "Error while committing with commit message $2"
-			return
-		fi
-		git push
-		if [ "$?" != "0" ]; then
-			echo "Error while pushing"
-			return
-		fi
-	elif [ -n "$1" ]; then
-		git add -A
-		if [ "$?" != "0" ]; then
-			echo "Error while adding all files"
-			return
-		fi
-		git commit -m $1
-		if [ "$?" != "0" ]; then
-			echo "Error while committing with commit message $1"
-			return
-		fi
-		git push
-		if [ "$?" != "0" ]; then
-			echo "Error while pushing"
-			return
-		fi
-	else
-		git add -A
-		if [ "$?" != "0" ]; then
-			echo "Error while adding all files"
-			return
-		fi
-		git commit -m "Automatic commit message"
-		if [ "$?" != "0" ]; then
-			echo "Error while committing with commit message \"Automatic Commit\""
-			return
-		fi
-		git push
-		if [ "$?" != 0 ]; then
-			echo "Error while pushing with commit message \"Automatic Commit\""
-			return
-		fi
+if [ -n "$2" ]; then
+	ga $1
+	if [ "$?" != "0" ]; then
+		echo "Error while adding file $1"
+		return
 	fi
-	echo "Everything went well !"
-}
+	gc -m $2
+	if [ "$?" != "0" ]; then
+		echo "Error while committing with commit message $2"
+		return
+	fi
+	gp
+	if [ "$?" != "0" ]; then
+		echo "Error while pushing"
+		return
+	fi
+elif [ -n "$1" ]; then
+	ga -A
+	if [ "$?" != "0" ]; then
+		echo "Error while adding all files"
+		return
+	fi
+	gc -m $1
+	if [ "$?" != "0" ]; then
+		echo "Error while committing with commit message $1"
+		return
+	fi
+	gp
+	if [ "$?" != "0" ]; then
+		echo "Error while pushing"
+		return
+	fi
+else
+	ga -A
+	if [ "$?" != "0" ]; then
+		echo "Error while adding all files"
+		return
+	fi
+	gc -m "Automatic commit"
+	if [ "$?" != "0" ]; then
+		echo "Error while committing with commit message \"Automatic Commit\""
+		return
+	fi
+	gp
+	if [ "$?" != 0 ]; then
+		echo "Error while pushing with commit message \"Automatic Commit\""
+		return
+	fi
+fi
